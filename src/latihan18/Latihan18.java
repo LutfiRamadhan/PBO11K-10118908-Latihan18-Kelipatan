0e11039e40bf64593e3f4b0fb064d808af352991
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-10-04 18:18:47 +0700

    First Commit

---
+++ src/latihan18/Latihan18.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan18;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 KARYAWAN
+ */
+public class Latihan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(3.5*i);
+        }
+    }
+    
+}
